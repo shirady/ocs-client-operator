de293f789c8599d3ce68537195958f6eead15ecb
shirady <57721533+shirady@users.noreply.github.com>
2026-02-19 15:52:16 +0200

    initial reconcile (just logs)
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>

---
+++ internal/controller/obc_controller.go
@@ -0,0 +1,65 @@
+package controller
+
+import (
+	"context"
+
+	obv1alpha1 "github.com/kube-object-storage/lib-bucket-provisioner/pkg/apis/objectbucket.io/v1alpha1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	"k8s.io/apimachinery/pkg/runtime"
+	ctrl "sigs.k8s.io/controller-runtime"
+	"sigs.k8s.io/controller-runtime/pkg/builder"
+	"sigs.k8s.io/controller-runtime/pkg/client"
+	"sigs.k8s.io/controller-runtime/pkg/event"
+	"sigs.k8s.io/controller-runtime/pkg/log"
+	"sigs.k8s.io/controller-runtime/pkg/predicate"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+// OBCReconciler reconciles a ObjectBucketClaim object
+type OBCReconciler struct {
+	client.Client
+	Scheme *runtime.Scheme
+}
+
+// SetupWithManager sets up the controller with the Manager.
+func (r *OBCReconciler) SetupWithManager(mgr ctrl.Manager) error {
+	// Only reconcile on Create and Delete events (not on Update).
+	createOrDeleteOnly := predicate.Funcs{
+		CreateFunc: func(event.CreateEvent) bool { return true },
+		UpdateFunc: func(event.UpdateEvent) bool { return false },
+		DeleteFunc: func(event.DeleteEvent) bool { return true },
+	}
+
+	return ctrl.NewControllerManagedBy(mgr).
+		Named("OBC").
+		For(
+			&obv1alpha1.ObjectBucketClaim{},
+			builder.WithPredicates(createOrDeleteOnly),
+		).
+		Complete(r)
+}
+
+//+kubebuilder:rbac:groups=objectbucket.io,resources=objectbucketclaims,verbs=get;list;watch
+
+func (r *OBCReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
+	log := log.FromContext(ctx)
+
+	obc := &obv1alpha1.ObjectBucketClaim{}
+	err := r.Get(ctx, req.NamespacedName, obc)
+	if err != nil {
+		if errors.IsNotFound(err) {
+			log.Info("OBC deleted", "namespace", req.Namespace, "name", req.Name)
+			return reconcile.Result{}, nil
+		}
+		log.Error(err, "failed to get ObjectBucketClaim")
+		return reconcile.Result{}, err
+	}
+
+	if obc.DeletionTimestamp != nil {
+		log.Info("OBC deleted", "namespace", obc.Namespace, "name", obc.Name)
+		return reconcile.Result{}, nil
+	}
+
+	log.Info("OBC created", "namespace", obc.Namespace, "name", obc.Name)
+	return reconcile.Result{}, nil
+}
