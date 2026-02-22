b6d0bcaaf8db3d6895436e4ca664a75d7c40d118
shirady <57721533+shirady@users.noreply.github.com>
2026-02-22 15:12:39 +0200

    use Notify gRPC call
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>

--- internal/controller/obc_controller.go
+++ internal/controller/obc_controller.go
@@ -2,10 +2,16 @@ package controller
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/go-logr/logr"
 	nbv1 "github.com/noobaa/noobaa-operator/v5/pkg/apis/noobaa/v1alpha1"
+	"github.com/red-hat-storage/ocs-client-operator/api/v1alpha1"
+	"github.com/red-hat-storage/ocs-client-operator/pkg/utils"
+	providerClient "github.com/red-hat-storage/ocs-operator/services/provider/api/v4/client"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -40,6 +46,7 @@ func (r *OBCReconciler) SetupWithManager(mgr ctrl.Manager) error {
 }
 
 //+kubebuilder:rbac:groups=objectbucket.io,resources=objectbucketclaims,verbs=get;list;watch
+//+kubebuilder:rbac:groups=ocs.openshift.io,resources=storageclients,verbs=get;list;watch
 
 func (r *OBCReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx).WithName("OBC")
@@ -49,6 +56,10 @@ func (r *OBCReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	if err != nil {
 		if errors.IsNotFound(err) {
 			log.Info("OBC deleted", "namespace", req.Namespace, "name", req.Name)
+			if err := r.notifyObcDeleted(ctx, log, req.NamespacedName); err != nil {
+				log.Error(err, "failed to notify provider of OBC deletion")
+				return reconcile.Result{}, err
+			}
 			return reconcile.Result{}, nil
 		}
 		log.Error(err, "failed to get ObjectBucketClaim")
@@ -57,9 +68,78 @@ func (r *OBCReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 
 	if obc.DeletionTimestamp != nil {
 		log.Info("OBC deleted", "namespace", obc.Namespace, "name", obc.Name)
+		if err := r.notifyObcDeleted(ctx, log, types.NamespacedName{Namespace: obc.Namespace, Name: obc.Name}); err != nil {
+			log.Error(err, "failed to notify provider of OBC deletion")
+			return reconcile.Result{}, err
+		}
 		return reconcile.Result{}, nil
 	}
 
 	log.Info("OBC created", "namespace", obc.Namespace, "name", obc.Name)
+	if err := r.notifyObcCreated(ctx, log, obc); err != nil {
+		log.Error(err, "failed to notify provider of OBC creation")
+		return reconcile.Result{}, err
+	}
 	return reconcile.Result{}, nil
+}
+
+// getStorageClientForNotify is a temporary function
+// until I can get the ownerReferences from the storageclass
+func (r *OBCReconciler) getStorageClientForNotify(ctx context.Context) (*v1alpha1.StorageClient, error) {
+	list := &v1alpha1.StorageClientList{}
+	if err := r.List(ctx, list); err != nil {
+		return nil, fmt.Errorf("list StorageClients: %w", err)
+	}
+	for i := range list.Items {
+		sc := &list.Items[i]
+		if sc.Status.ConsumerID != "" && sc.Spec.StorageProviderEndpoint != "" {
+			return sc, nil
+		}
+	}
+	return nil, fmt.Errorf("no StorageClient with ConsumerID and StorageProviderEndpoint found")
+}
+
+func (r *OBCReconciler) notifyObcCreated(ctx context.Context, log logr.Logger, obc *nbv1.ObjectBucketClaim) error {
+	storageClient, err := r.getStorageClientForNotify(ctx)
+	if err != nil {
+		return err
+	}
+	pc, err := NewProviderClientForStorageClient(ctx, storageClient)
+	if err != nil {
+		return fmt.Errorf("create provider client: %w", err)
+	}
+	defer pc.Close()
+	_, err = pc.NotifyObcCreated(ctx, storageClient.Status.ConsumerID, obc)
+	if err != nil {
+		return fmt.Errorf("NotifyObcCreated: %w", err)
+	}
+	log.Info("notified provider of OBC creation", "namespace", obc.Namespace, "name", obc.Name)
+	return nil
+}
+
+func (r *OBCReconciler) notifyObcDeleted(ctx context.Context, log logr.Logger, nn types.NamespacedName) error {
+	storageClient, err := r.getStorageClientForNotify(ctx)
+	if err != nil {
+		return err
+	}
+	pc, err := NewProviderClientForStorageClient(ctx, storageClient)
+	if err != nil {
+		return fmt.Errorf("create provider client: %w", err)
+	}
+	defer pc.Close()
+	_, err = pc.NotifyObcDeleted(ctx, storageClient.Status.ConsumerID, nn)
+	if err != nil {
+		return fmt.Errorf("NotifyObcDeleted: %w", err)
+	}
+	log.Info("notified provider of OBC deletion", "namespace", nn.Namespace, "name", nn.Name)
+	return nil
+}
+
+// NewProviderClientForStorageClient creates an OCS provider gRPC client for the given StorageClient.
+func NewProviderClientForStorageClient(ctx context.Context, sc *v1alpha1.StorageClient) (*providerClient.OCSProviderClient, error) {
+	pc, err := providerClient.NewProviderClient(ctx, sc.Spec.StorageProviderEndpoint, utils.OcsClientTimeout)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create provider client with endpoint %v: %w", sc.Spec.StorageProviderEndpoint, err)
+	}
+	return pc, nil
 }
