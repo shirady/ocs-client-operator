8ae891ddd402683d2def4cef48ebfac0a1d94fa0
shirady <57721533+shirady@users.noreply.github.com>
2026-03-10 08:36:34 +0200

    add label of storageClient on the OBC
    will be used in deletion phase of the storage client before calling offboarding
    
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>

--- internal/controller/obc_controller.go
+++ internal/controller/obc_controller.go
@@ -141,11 +141,20 @@ func (r *obcReconcile) createOrUpdatePhase(
 	storageClient *v1alpha1.StorageClient,
 ) (ctrl.Result, error) {
 	r.log.Info("OBC created/updated", "namespaced/name", client.ObjectKeyFromObject(&r.obc))
+
+	shouldUpdateMetaData := false
 	if controllerutil.AddFinalizer(&r.obc, ObcFinalizer) {
 		r.log.Info("Finalizer not found for OBC. Adding finalizer", "namespaced/name", client.ObjectKeyFromObject(&r.obc))
+		shouldUpdateMetaData = true
+	}
+	// this label is used to identify the StorageClient that the OBC is associated with
+	if utils.AddLabel(&r.obc, storageClientNameLabel, storageClient.Name) {
+		shouldUpdateMetaData = true
+	}
+	if shouldUpdateMetaData {
 		if err := r.Update(r.ctx, &r.obc); err != nil {
-			r.log.Info("Failed to add finalizer to OBC", "namespaced/name", client.ObjectKeyFromObject(&r.obc))
-			return reconcile.Result{}, fmt.Errorf("failed to add finalizer to OBC: %v", err)
+			r.log.Info("Failed to update OBC (finalizer/label)", "namespaced/name", client.ObjectKeyFromObject(&r.obc))
+			return reconcile.Result{}, fmt.Errorf("failed to update OBC: %v", err)
 		}
 		return reconcile.Result{Requeue: true}, nil
 	}
