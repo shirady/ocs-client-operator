cef022f546735986d1a2d4146f6b62d8827dd253
shirady <57721533+shirady@users.noreply.github.com>
2026-03-29 10:41:34 +0300

    obc controller will be set only when CRD us installed
    Signed-off-by: shirady <57721533+shirady@users.noreply.github.com>

--- cmd/main.go
+++ cmd/main.go
@@ -337,12 +337,14 @@ func main() {
 		}
 	}
 
-	if err = (&controller.ObcReconciler{
-		Client: mgr.GetClient(),
-		Scheme: mgr.GetScheme(),
-	}).SetupWithManager(mgr); err != nil {
-		setupLog.Error(err, "unable to create controller", "controller", "ObjectBucketClaim")
-		os.Exit(1)
+	if availCrds[controller.ObjectBucketClaimCrdName] {
+		if err = (&controller.ObcReconciler{
+			Client: mgr.GetClient(),
+			Scheme: mgr.GetScheme(),
+		}).SetupWithManager(mgr); err != nil {
+			setupLog.Error(err, "unable to create controller", "controller", "ObjectBucketClaim")
+			os.Exit(1)
+		}
 	}
 
 	setupLog.Info("starting manager")
